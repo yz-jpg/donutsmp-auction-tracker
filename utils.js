80af2bd615a9ff35ae28682e1d347a96a0b5984c
yz-jpg <s10258968@connect.np.edu.sg>
2026-03-14 16:56:43 +0800

    made the utils more modern

--- utils.js
+++ utils.js
@@ -1,21 +1,18 @@
-import { raw } from "express"
+export const parseItem = (rawItem) => {
+        const id = rawItem.item.id;
+        const count = Number(rawItem.item.count);
+        const totalPrice = Number(rawItem.price);
+        const timestamp = rawItem.unixMillisDateSold;
 
-const parseItem = (rawItem) => {
-    const id = rawItem.item.id;
-    const count = Number(rawItem.item.count);
-    const totalPrice = Number(rawItem.price);
-    const timestamp = rawItem.unixMillisDateSold;
+        const priceEach = count > 0 ? totalPrice / count : 0;
 
-    const priceEach = count > 0 ? totalPrice / count : 0;
-
-    return {
-        item_id: id.includes(':') ? id.split(':')[1] : id,
-        item_count: count,
-        price_total: totalPrice,
-        price_each: priceEach,
-        sold_timestamp: timestamp,
-        sold_at: new Date(timestamp)
+        return {
+            item_id: id.includes(':') ? id.split(':')[1] : id,
+            item_count: count,
+            price_total: totalPrice,
+            price_each: priceEach,
+            sold_timestamp: timestamp,
+            sold_at: new Date(timestamp)
+        };
     };
-};
 
-module.exports = { parseItem };
